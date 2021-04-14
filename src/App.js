500a85f2da4f35b0ee0869ec4fdaaba64db9a509
Caleb Ventura <caleb.ventura.v@uni.pe>
2021-04-14 12:36:27 -0500

    Primer commite de la aplicacion react-firebase

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <p>Hola mundo mundial</p>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDBvQJbiezr7NkXtwmNt2qmSC1J0-2sQsc",
+    authDomain: "fr-crud.firebaseapp.com",
+    projectId: "fr-crud",
+    storageBucket: "fr-crud.appspot.com",
+    messagingSenderId: "870101594664",
+    appId: "1:870101594664:web:a176b8737c631b58d63b1d"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
